3dfc6a42bd05ac5a3292f175556510768bcaac65
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-18 08:54:18 -0800

    laying out testing and class for order

---
+++ PierresVendorMgmt.Tests/ModelsTests/OrderTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendorMgmt.Models;
+using System.Collections.Generic;
+using System;
+namespace PierresVendorMgmt.Tests
+{
+  [TestClass]
+  public class OrderTest
+  {
+
+  }
+}
