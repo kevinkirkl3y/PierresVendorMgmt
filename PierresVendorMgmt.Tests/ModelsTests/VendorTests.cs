e03942431bbb924b94264932022fb3a32fb01d49
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-18 12:41:30 -0800

    create test of constructor returning vendor properties.

--- PierresVendorMgmt.Tests/ModelsTests/VendorTests.cs
+++ PierresVendorMgmt.Tests/ModelsTests/VendorTests.cs
@@ -17,5 +17,14 @@ namespace PierresVendorMgmt.Tests
       Vendor newVendor = new Vendor();
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void VendorConstructor_CorrectlyReturnsPropertiesOfVendors_VendorProperties()
+    {
+      string name = "Suzies Cafe";
+      string description = "Small Cafe";
+      Vendor newVendor = new Vendor(name, description);
+      Assert.AreEqual(name, newVendor.Name);
+      Assert.AreEqual(description, newVendor.Description);
+    }
   }
 }
