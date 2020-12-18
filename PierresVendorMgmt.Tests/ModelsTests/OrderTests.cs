278d7cbea524bd853977d8b59f2f6b76719ffebd
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-18 09:35:32 -0800

    create test for Order constructor.

--- PierresVendorMgmt.Tests/ModelsTests/OrderTests.cs
+++ PierresVendorMgmt.Tests/ModelsTests/OrderTests.cs
@@ -13,5 +13,19 @@ namespace PierresVendorMgmt.Tests
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void OrderConstructor_CorrectlyReturnsPropertiesOfOrder_Properties()
+    {
+      string name = "Test";
+      string description = "Test";
+      int price = 1;
+      string date = "12.18.2020";
+      Order newOrder = new Order(name, description, price, date);
+      Assert.AreEqual(name, newOrder.Name);
+      Assert.AreEqual(description, newOrder.Description);
+      Assert.AreEqual(price, newOrder.Price);
+      Assert.AreEqual(date, newOrder.Date);
+    }
+
   }
 }
--- PierresVendorMgmt/Models/Order.cs
+++ PierresVendorMgmt/Models/Order.cs
@@ -4,6 +4,11 @@ namespace PierresVendorMgmt.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; set; }
+    private static List<Order> _instances = new List<Order>{};
   }
 }
