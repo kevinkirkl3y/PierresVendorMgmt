e183a55d2377152ad2eb848605c7f170f6b04f8e
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-18 10:58:30 -0800

    Create Find method.

--- PierresVendorMgmt/Models/Order.cs
+++ PierresVendorMgmt/Models/Order.cs
@@ -28,6 +28,10 @@ namespace PierresVendorMgmt.Models
     {
       _instances.Clear();
     }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
     
 
   }
